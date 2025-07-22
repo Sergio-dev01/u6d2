1b9a4fa59ae3984ec04cf9d934700b5e28399638
Sergio-dev01 <sergio.develoop@gmail.com>
2025-07-22 17:08:49 +0200

    add toString and costructor

--- src/main/java/sergiomaselli/u6d2/payloads/NewBlogPostPayload.java
+++ src/main/java/sergiomaselli/u6d2/payloads/NewBlogPostPayload.java
@@ -1,13 +1,25 @@
 package sergiomaselli.u6d2.payloads;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class NewBlogPostPayload {
     private String categoria;
     private String titolo;
     private String contenuto;
     private int tempoDiLettura;
+
+    @Override
+    public String toString() {
+        return "NewBlogPostPayload{" +
+                "categoria='" + categoria + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", contenuto='" + contenuto + '\'' +
+                ", tempoDiLettura=" + tempoDiLettura +
+                '}';
+    }
 }
