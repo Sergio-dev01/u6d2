62d65697d1916f8382c5e8c4dc060bcec6168246
Sergio-dev01 <sergio.develoop@gmail.com>
2025-07-22 17:08:03 +0200

    add mod payload

--- src/main/java/sergiomaselli/u6d2/payloads/NewAuthorPayload.java
+++ src/main/java/sergiomaselli/u6d2/payloads/NewAuthorPayload.java
@@ -1,5 +1,6 @@
 package sergiomaselli.u6d2.payloads;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +8,22 @@ import java.time.LocalDate;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class NewAuthorPayload {
     private String nome;
     private String cognome;
     private String email;
     private LocalDate dataDiNascita;
+
+    @Override
+    public String toString() {
+        return "NewAuthorPayload{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataDiNascita=" + dataDiNascita +
+                '}';
+    }
 }
+
+
